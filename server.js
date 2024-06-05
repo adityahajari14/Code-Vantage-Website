0a953f808946a868f529d0830ad761e8274a77f1
Aditya Hazari <72179316+adityahajari14@users.noreply.github.com>
2024-06-05 21:51:18 +0530

    Fixed Backend

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
-import path from "path"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
